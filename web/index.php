63d53f5bf223c990f2292f0da940ccfdfded7b98
Gunnar Lium <gunnar@aptoma.com>
2014-01-28 22:26:35 +0100

    Update recommended Symfony and Silex versions.

--- web/index.php
+++ web/index.php
@@ -1,10 +1,11 @@
 <?php
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
-use Symfony\Component\HttpKernel\Debug\ErrorHandler;
-ErrorHandler::register(); // Convert errors to exceptions
+\Symfony\Component\Debug\ErrorHandler::register(); // Convert errors to exceptions
 
 $app = require __DIR__ . '/../app/app.php';
+
 if ($app instanceof Silex\Application) {
     $app->run();
 }
