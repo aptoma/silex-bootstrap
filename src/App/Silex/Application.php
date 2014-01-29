3f4719aecd6081b5b4ef340c423609c16dee4719
Gunnar Lium <gunnar@aptoma.com>
2014-01-29 10:04:11 +0100

    Move debug error handler init Application constructor.

--- src/App/Silex/Application.php
+++ src/App/Silex/Application.php
@@ -3,12 +3,16 @@
 namespace App\Silex;
 
 use Aptoma\Silex\Application as BaseApplication;
+use Symfony\Component\Debug\Debug;
 
 class Application extends BaseApplication
 {
 
     public function __construct(array $values = array())
     {
+        if ($values['debug']) {
+            Debug::enable();
+        }
         parent::__construct($values);
 
         $this->registerLogger($this);
--- web/index.php
+++ web/index.php
@@ -1,11 +1,9 @@
 <?php
 
-require_once __DIR__ . '/../vendor/autoload.php';
-
-\Symfony\Component\Debug\ErrorHandler::register(); // Convert errors to exceptions
-
 $app = require __DIR__ . '/../app/app.php';
 
 if ($app instanceof Silex\Application) {
     $app->run();
+} else {
+    die('Failed to initialize application.');
 }
